42a8682aaffcfdf6bdfef4b2657b8c2d5740a2b0
ntombikayisemakola <ntombikayise.makola@gmail.com>
2023-08-15 20:13:00 -0700

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main -Entry point
@@ -9,5 +10,23 @@
 
 int main(void)
 {
-	int count
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
 
