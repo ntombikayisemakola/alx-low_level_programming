3a35afcbb6af161454f90613fb318de4d7337288
ntombikayisemakola <ntombikayise.makola@gmail.com>
2023-08-16 01:36:32 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_times_table - prints time table of n
@@ -31,9 +32,9 @@ void print_times_table(int n)
 				{
 					_putchar((prod / 100) + 48);
 					_putchar((prod / 10) % 10 + 48);
-				}
-				else if (prod <= 99 && prod >= 10)
+				} else if (prod <= 99 && prod >= 10)
 					_putchar((prod % 10) + 48);
+				_putchar((prod % 10) + 48);
 			}
 			_putchar('\n');
 		}
