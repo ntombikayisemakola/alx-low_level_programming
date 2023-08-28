c1b83f8ce4b67b962b2effd19cc15a5bdf39a52e
ntombikayisemakola <ntombikayise.makola@gmail.com>
2023-08-28 05:54:46 -0700

    Task 1. memcpy

--- 0x07-pointers_arrays_strings/1-main.c
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -13,19 +13,19 @@ void simple_print_buffer(char *buffer, unsigned int size)
 unsigned int i;
 
 i = 0;
-	while (i < size)
-	{
+while (i < size)
+{
 	if (i % 10)
 	{
 		printf(" ");
 	}
 	if (!(i % 10) && i)
-	}
-		printf("\n");
+	{
+	printf("\n");
 	}
 	printf("0x%02x", buffer[i]);
 	i++;
-	}
+}
 	printf("\n");
 }
 
